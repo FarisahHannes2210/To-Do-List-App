61860928b41cc3f54fae92b334d18bbec77b7914
FarisahHannes2210 <Hannesfarisah@gmail.com>
2025-01-13 02:11:02 +0800

    all trigger js

---
+++ action.js
@@ -0,0 +1,9 @@
+function add(){
+
+    var task = document.getElementById('add').value;
+
+    document.getElementById('list').innerHTML += '<li>' + task + '</li>';
+    //innerhtml = input value
+
+
+}
